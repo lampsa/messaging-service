c884e078dafdee22bb5e675b246629465582a1c2
Jani Lampinen <jani.lampinen@hotmail.com>
2023-06-12 20:05:47 +0300

    added comments for the wrapper fields

--- src/main/java/fi/techappeal/messagingservice/SendMessageWrapper.java
+++ src/main/java/fi/techappeal/messagingservice/SendMessageWrapper.java
@@ -2,6 +2,9 @@ package fi.techappeal.messagingservice;
 
 import java.util.Map;
 
+/**
+ * Wraps a message that is sent to messaging service in a cloud-agnostic way.
+ */
 public class SendMessageWrapper {
     private final String payload;
     private final Map<String, String> attributes;
@@ -11,10 +14,18 @@ public class SendMessageWrapper {
         this.attributes = attributes;
     }
 
+    /**
+     * Returns the attributes of the message.
+     * @return
+     */
     public Map<String, String> getAttributes() {
         return attributes;
     }
 
+    /**
+     * Returns the payload of the message.
+     * @return payload of the message
+     */
     public String getPayload() {
         return payload;
     }
