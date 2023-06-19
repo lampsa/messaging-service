68223cf06789dc0192a0504709cae246a9237ad3
Jani Lampinen <jani.lampinen@hotmail.com>
2023-06-19 14:23:38 +0300

    added test case for receiving multiple messages

--- src/test/java/fi/techappeal/messagingservice/sqs/SqsMessageReceiverTest.java
+++ src/test/java/fi/techappeal/messagingservice/sqs/SqsMessageReceiverTest.java
@@ -12,6 +12,7 @@ import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -33,7 +34,7 @@ class SqsMessageReceiverTest {
      * Test that the cloud-specific message is correctly mapped to a cloud-agnostic message.
      */
     @Test
-    void receiveMessage_correctResponses() {
+    void receiveMessage_correctResponse() {
         String queueName = "MyQ";
         String queueUrl = "mocked";
 
@@ -60,6 +61,45 @@ class SqsMessageReceiverTest {
             assertEquals("123", message.getId());
             assertEquals("value1", message.getAttributes().get("attr1"));
             receiver.stop();
+            return ProcessingState.PROCESSED;
+        });
+    }
+
+    /**
+     * Test that multiple cloud-specific messages are correctly mapped to a cloud-agnostic messages.
+     */
+    @Test
+    void receiveMessages_multiple() {
+        String queueName = "MyQ";
+        String queueUrl = "mocked";
+        Map<String, MessageAttributeValue> attributes1 = Map.of(
+                "attr1", MessageAttributeValue.builder().stringValue("value1").build()
+        );
+        Map<String, MessageAttributeValue> attributes2 = Map.of(
+                "attr1", MessageAttributeValue.builder().stringValue("value2").build()
+        );
+        Map<String, MessageAttributeValue> attributes3 = Map.of(
+                "attr1", MessageAttributeValue.builder().stringValue("value3").build()
+        );
+        List<Message> messages = List.of(
+                Message.builder().body("message1").messageId("123").messageAttributes(attributes1).build(),
+                Message.builder().body("message2").messageId("456").messageAttributes(attributes2).build(),
+                Message.builder().body("message3").messageId("789").messageAttributes(attributes3).build()
+        );
+        ReceiveMessageResponse mockReceiveMessageResponse = ReceiveMessageResponse.builder()
+                .messages(messages)
+                .build();
+        when(mockSqsClient.receiveMessage(any(ReceiveMessageRequest.class))).thenReturn(mockReceiveMessageResponse);
+        receiver.setQueueUrlCache(queueName, queueUrl);
+        AtomicInteger count = new AtomicInteger();
+        receiver.subscribe("MyQ", message -> {
+            assertEquals(messages.get(count.get()).body(), message.getPayload());
+            assertEquals(messages.get(count.get()).messageId(), message.getId());
+            assertEquals(messages.get(count.get()).messageAttributes().get("attr1").stringValue(), message.getAttributes().get("attr1"));
+            count.addAndGet(1);
+            if (count.get() == 3) {
+                receiver.stop();
+            }
             return ProcessingState.PROCESSED;
         });
     }
